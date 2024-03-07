3185d7af73f3fad4a2b23f1a1c56f83f49fffec9
Bencsik Boglár Bianka <bencsikboglarbianka@ktch.hu>
2024-03-07 09:57:17 +0100

    Befejezés

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+/*
+* File: app.js
+* Author:  Bencsik Boglár Bianka
+* Copyright: 2024, Bencsik Boglár Bianka 
+* Group: Szoft I-1-N
+* Date: 2024-03-07
+* Github: https://github.com/BencsikBoglarBianka/hurnegyter.git
+*/
+
+
 const AsideInput = document.querySelector("#Aside")
 const BsideInput = document.querySelector("#Bside")
 const CsideInput = document.querySelector("#Cside")
 const DsideInput = document.querySelector("#Dside")
 
 const perimeterInput = document.querySelector("#perimeter")
+
 const calcButton = document.querySelector("#calcbutton")
 
 calcButton.addEventListener('click', () => { 
@@ -11,13 +22,22 @@ calcButton.addEventListener('click', () => {
 });
 
 function startCalc(){
-    const Aside = AsideInput.value
-    const Bside = BsideInput.value
-    const Cside = CsideInput.value
-    const Dside = DsideInput.value
+    const Aside = Number(AsideInput.value)
+    const Bside = Number(BsideInput.value)
+    const Cside = Number(CsideInput.value)
+    const Dside = Number(DsideInput.value)
     const felkerulet = (Aside+ Bside +Cside+ Dside)/2
-    const terulet =(Math.sqrt((felkerulet - Aside)*(felkerulet-Bside)*(felkerulet-Cside)*(felkerulet-Dside)))
+    const terulet =Math.sqrt((felkerulet - Aside)*(felkerulet-Bside)*(felkerulet-Cside)*(felkerulet-Dside))
+    console.log("Területe: ", terulet)
 
+    // perimeterInput = calcPerimeter()
     perimeterInput.value = terulet
 }
+
+// function calcPerimeter(){
+//     const felkerulet = (Aside+ Bside +Cside+ Dside)/2
+//     const terulet =(Math.sqrt((felkerulet - Aside)*(felkerulet-Bside)*(felkerulet-Cside)*(felkerulet-Dside)))
+
+
+// }
 
